8515088556b60c987117a217a32a15aff45eebc5
mgil <developer.magd.89@gmail.com>
2023-06-28 20:05:21 +0200

    [T-13] Remove singular client

--- client/src/features/ui/badges/Counter.tsx
+++
@@ -1,7 +0,0 @@
-export const Counter = ({num = 0}) => (
-    <div className="p-2">
-      <p className="flex h-2 w-2 items-center justify-center rounded-lg bg-indigo-500 p-3 text-xs text-white">
-        {num}
-      </p>
-    </div>
-);
